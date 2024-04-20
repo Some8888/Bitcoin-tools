efae047a3f99870384fa9b3037f3c3c526d8d7b2
Cole <167555279+Some8888@users.noreply.github.com>
2024-04-20 13:19:19 +0300

    btc_pubkey.py

--- btc_pubkey.py
+++ btc_pubkey.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-#			(tips)  If this was useful to you.
-
-#                 BTC:	bc1qnqtqyqfu9rntykn299sp3pydr2vn3khwcvd58t
-#                ETH:	0x471c21cD1a37994636cc3e588E57ccfF252c9f57
-#    (TRC-20)  USDT:	TH4NgnVWR4mqNH5bgC5aq4qQqmnXdh87v3
-
-#					BTC Pubkey.
-#			(only if there are outgoing transactions.)
+# BTC Pubkey. (only if there are outgoing transactions.)
 
 import os
 import requests
