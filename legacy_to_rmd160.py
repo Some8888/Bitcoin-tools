8e088273a1337f3d277695ae7f575fc9eba15199
Cole <167555279+Some8888@users.noreply.github.com>
2024-04-20 13:21:02 +0300

    legacy_to_rmd160.py

--- legacy_to_rmd160.py
+++ legacy_to_rmd160.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-#			(tips)  If this was useful to you.
-
-#                 BTC:	bc1qnqtqyqfu9rntykn299sp3pydr2vn3khwcvd58t
-#                ETH:	0x471c21cD1a37994636cc3e588E57ccfF252c9f57
-#    (TRC-20)  USDT:	TH4NgnVWR4mqNH5bgC5aq4qQqmnXdh87v3
-
-#			Convert all BTC addr from file "Legacy" to rmd160
-#                                  (Compressed btc addr only)
+# Convert all BTC addr from file "Legacy" to rmd160 (Compressed btc addr only)
 
 import base58
 import time
