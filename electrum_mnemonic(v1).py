dfc7ec4dd08631e95caa79c719d4f3c347d64b0a
Cole <167555279+Some8888@users.noreply.github.com>
2024-04-25 15:42:12 +0300

    electrum_mnemonic(v1).py

--- electrum_mnemonic(v1).py
+++ electrum_mnemonic(v1).py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python3
-
-# Convert old Electrum mnemonic(v1) to addr and priv.
-
-import hashlib
+from typing import Union
 import base58
 import ecdsa
-import binascii
+from bip_utils.utils.crypto.ripemd import Ripemd160
+from bip_utils.utils.crypto.sha2 import Sha256
 from bip_utils import ElectrumV1, ElectrumV1SeedGenerator, ElectrumV1Languages
 
 BLUE = "\033[94m"
 RESET = "\033[0m"
+
+class Hash160:
+    @staticmethod
+    def QuickDigest(data: Union[bytes, str]) -> bytes:
+        return Ripemd160.QuickDigest(Sha256.QuickDigest(data))
 
 mnemonic = input("Mnemonic: ")
 seed_bytes = ElectrumV1SeedGenerator(mnemonic, ElectrumV1Languages.ENGLISH).Generate()
@@ -21,22 +23,18 @@ verifying_key = signing_key.get_verifying_key()
 public_key_compressed = verifying_key.to_string("compressed")
 public_key_uncompressed = verifying_key.to_string("uncompressed")
 
-hash_sha256_step2_compressed = hashlib.sha256(public_key_compressed).digest()
-hash_ripemd160_step3_compressed = hashlib.new('ripemd160', hash_sha256_step2_compressed).digest()
+hash_sha256_step2_compressed = Hash160.QuickDigest(public_key_compressed)
 version_byte = b'\x00'
-hash_with_version_compressed = version_byte + hash_ripemd160_step3_compressed
-hash_sha256_step5_compressed = hashlib.sha256(hash_with_version_compressed).digest()
-hash_sha256_step6_compressed = hashlib.sha256(hash_sha256_step5_compressed).digest()
-checksum_compressed = hash_sha256_step6_compressed[:4]
+hash_with_version_compressed = version_byte + hash_sha256_step2_compressed
+hash_sha256_step5_compressed = Hash160.QuickDigest(hash_with_version_compressed)
+checksum_compressed = hash_sha256_step5_compressed[:4]
 address_hex_compressed = hash_with_version_compressed + checksum_compressed
 bitcoin_address_compressed = base58.b58encode(address_hex_compressed).decode()
 
-hash_sha256_step2_uncompressed = hashlib.sha256(public_key_uncompressed).digest()
-hash_ripemd160_step3_uncompressed = hashlib.new('ripemd160', hash_sha256_step2_uncompressed).digest()
-hash_with_version_uncompressed = version_byte + hash_ripemd160_step3_uncompressed
-hash_sha256_step5_uncompressed = hashlib.sha256(hash_with_version_uncompressed).digest()
-hash_sha256_step6_uncompressed = hashlib.sha256(hash_sha256_step5_uncompressed).digest()
-checksum_uncompressed = hash_sha256_step6_uncompressed[:4]
+hash_sha256_step2_uncompressed = Hash160.QuickDigest(public_key_uncompressed)
+hash_with_version_uncompressed = version_byte + hash_sha256_step2_uncompressed
+hash_sha256_step5_uncompressed = Hash160.QuickDigest(hash_with_version_uncompressed)
+checksum_uncompressed = hash_sha256_step5_uncompressed[:4]
 address_hex_uncompressed = hash_with_version_uncompressed + checksum_uncompressed
 bitcoin_address_uncompressed = base58.b58encode(address_hex_uncompressed).decode()
 
@@ -44,11 +42,11 @@ wif_compressed = base58.b58encode_check(b'\x80' + private_key + b'\x01')
 wif_uncompressed = base58.b58encode_check(b'\x80' + private_key)
 
 print("\n")
-print(f"	BTC: {bitcoin_address_compressed} {BLUE}Сompressed{RESET}")
-print(f"	WIF: {wif_compressed.decode()}")
+print(f"    BTC: {bitcoin_address_compressed} {BLUE}Сompressed{RESET}")
+print(f"    WIF: {wif_compressed.decode()}")
 print("\n")
-print(f"	BTC: {bitcoin_address_uncompressed} {BLUE}Uncompressed{RESET}")
-print(f"	WIF: {wif_uncompressed.decode()}")
+print(f"    BTC: {bitcoin_address_uncompressed} {BLUE}Uncompressed{RESET}")
+print(f"    WIF: {wif_uncompressed.decode()}")
 print("\n")
-print(f"	HEX: {private_key_hex}")
+print(f"    HEX: {private_key_hex}")
 print("\n")
